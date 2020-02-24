f01ab1baa8e58dfdd7d59558ebb13b131d17b636
itsnotgoodname <gurnaindeol@gmail.com>
2020-02-24 14:01:09 -0800

    Format

--- src/index.js
+++ src/index.js
@@ -1,35 +1,34 @@
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
-const mongoConnection = require('./config/db').connection
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
 
 const app = express();
 
 // Views
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // Disable
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 
 // Middleware
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  express.urlencoded({
     extended: false
-}));
+  })
+);
 
-if (process.env.NODE_ENV === 'production') {
-    // Production
+if (process.env.NODE_ENV === "production") {
+  // Production
 } else {
-    // Development
-    app.use(morgan('dev'));
+  // Development
+  app.use(morgan("dev"));
 }
 
 // Controllers
-const {
-    homeController,
-} = require('./controllers');
+const { homeController } = require("./controllers");
 
-app.use('/', homeController);
+app.use("/", homeController);
 
-module.exports = app
+module.exports = app;
